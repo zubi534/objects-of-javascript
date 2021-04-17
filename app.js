9cb3f60790862aca95845a0ebfd4f2f1f784adf8
zubi534 <81070198+zubi534@users.noreply.github.com>
2021-04-17 21:27:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,630 @@
+ /* chapter_1 (Alert)
+//  Q1.  
+
+
+ alert("Hello! How are you?");  */
+
+//  Q2. 
+
+/* alert("Error! Please enter a valid password."); */
+
+// Q3   
+
+
+
+/* alert("Welcome to JS Land... \n Happy Coding!"); */
+
+
+// Q4   
+
+
+
+/* alert("Welcome to JS Land..."); */
+/* alert("Happy Coding! \n Prevent this page from creating additional dialogs."); */
+
+
+// Q5.   
+
+
+/* console.log("hello .. I can run JS through my web browser's console"); */
+
+
+
+// chapter 2
+
+
+// Q1.    
+
+
+
+// var a ;
+
+// Q2. 
+
+
+/* var my_name = "zubaida kanwal";
+ document.write(my_name); */
+
+// Q3.
+
+
+
+ /* var message ="Hello World";
+ alert(message); */
+
+
+// Q4.
+
+
+
+ /* var student_name="Zubaida Kanwal"
+ alert(student_name); */
+
+
+/*var student_age="15 years old"
+alert(student_age);*/
+
+ /* var course_name="web and App Development course"
+ alert(course_name); */
+
+// Q5
+
+
+/* var pizza="PIZZA \n PIZZ \n PIZ \n PI \n P"
+ alert(pizza); */
+
+// Q6
+
+
+/* var email="abc@example.com"
+ alert("My email address is "+email); */
+
+
+// Q6
+
+
+/* var book="A smarter way to learn JavaScript."
+ alert(book); */
+
+// Q7
+
+
+/* var JavaScript="Yah! I can write html content through javascript"
+ document.write(JavaScript); */
+
+// Q8 
+
+
+/* var end="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+ alert(end); */
+
+// chapter 3 (Variables for Numbers)
+
+// Q1
+
+
+/* var age= 20;
+ alert(" I am " +age+" years old "); */
+
+// Q2
+
+
+/* var visit= 10;
+ alert(" You have visited this site " +visit+" times "); */
+
+// Q3
+
+
+ /* var birth_year= 1999;
+ document.write("My birth year is " +birth_year); */
+
+// Q4 (a,b,c)
+
+
+/*var visitor_name="Ali"
+var quantity= 15;
+var product_title="shirt"
+
+document.write(visitor_name+ " ordered "  + quantity +" "+ product_title + " on xyz clothing store ") */
+
+
+// Chapter 4 ( Variables names legal and illegal)
+
+// Q1
+
+
+  /* var name="zubaida kanwal",father="Ali Akbar",address="Site area";
+  document.write(name+"  " +father+ " " +address) */
+
+// Q2
+
+
+/* legal variable
+var _zubaida,$zubaida,zubi,ZUBAIDA,zubaida_kanwal
+ illegal variable
+var 12ali,123,++ali,#ali,%ali */
+
+// Q3 (a,b,c,d,e)
+
+
+/* document.write("<h1> Rules for naming js variable </h1> ");
+document.write("<P> Variable names can only contain numbers,$_ . For example : $my_1st variable </p> " );
+document.write("<p> Variables must begin with a letter, $ or _. For example $name, _name or name</p>") ;
+document.write("<p> Variable names are case sensitive </p>");
+document.write("<p> Variable names should not be JS  keywords</P>"); */
+
+
+
+// Chapter 5 (Math Expression)
+
+
+// Q1
+
+
+/* var a= 5;
+ var b= 3;
+ var c= a+b;
+ document.write(" Sum of  " +b+ " and "  +a+ " is " +c); */
+
+
+// Q2(subraction,multiplication,division,modules)
+
+
+
+ /* var a= 5;
+ var b= 3;
+ var c= a-b;
+ document.write(" After subraction answer is " +c); */
+
+/* var a= 5;
+ var b= 3;
+ var c= a*b;
+ document.write (" After multiplication answer is "+c); */
+
+/* var a= 5;
+ var b= 3;
+ var c= a/b;
+ document.write(" After division answer is " +c); */
+
+/* var a= 5;
+ var b= 3;
+ var c= a%b;
+ document.write(" After modulus answer is " +c); */
+
+// Q3(a,b,c,d,e,f,g,h,i,j,k,l,)
+
+ /* var a;
+  document.write(" value after declaration is " +a); */
+
+
+  /* var a="5"
+   document.write("initial value is " +a); */
+
+
+ /* var a= 5;
+ document.write(a); */
+
+   /* var a= 5;
+    ++a;
+ document.write( "value after increment is "+a); */
+
+  /* a=a+7;
+ document.write("<p> <br> value after adding is "+a+" <p>") */
+
+ /* --a;
+ document.write("value after decrement is " +a); */
+
+ /* a=a%3;
+ document.write(" after dividing the variable ,the reminder is " +a); */
+
+
+//  Q4 
+
+
+ /* var ticket_cost= 600;
+ var ticket= 5;
+ var total = 600*5;
+ document.write("Total cost to buy " +ticket+" tickets to a movie is " +total); */
+
+ /* Q5 
+
+
+var i;
+for(i=1;i<11;i++)
+{
+    document.write("4 * "+i+" = "+4*i+"<br>")
+} */
+
+// Q6 
+
+
+/* var c=25; f=77;
+c=(f-32)*5/9;
+document.write(c+ " C is "+f+ " F "  ); 
+
+var c=21.111; f=70;
+f=(c*9/5)+32;
+document.write(c+ " C is "+f+ " F " ); */
+
+
+//  Q7 
+
+
+/* var price_item_1= 650;
+var quantity_item_1= 3;
+var price_item_2= 100;
+var quantity_item_2= 7;
+var shipping_charges= 100;
+var total_cost= ((price_item_1*quantity_item_1)+(price_item_2*quantity_item_2)+shipping_charges);
+document.write("<h1>  Shopping cart </h1> ");
+document.write("<p>   Price of item 1 is "  +price_item_1+" </p>"); 
+document.write("<p> Quantity of item 1 is " +quantity_item_1+" </p>");
+document.write("<p> Price pf item 2 is " +price_item_2+" </p>");
+document.write("<p> Quantity of item 2 is " +quantity_item_2+" </p>");
+document.write("<p> Total cost of your order is " +total_cost+" </p>"); */
+
+
+//  Q8 
+
+
+ /* var total_marks= 980;
+var obtained_marks= 804;
+var percentage= 804/980*100
+document.write( " <h1> Marksheet  </h1> ")
+document.write(" <P> Total marks: " +total_marks+ " </P> ")
+document.write( " <p> Marks Obtained: " +obtained_marks+ " </p> ")
+document.write( " <p> Percentage: " +percentage+ "% </p> ") */
+
+//  Q9 
+
+
+/* var dollar= 10;
+var saudi_riyal= 25;
+var pkr=(dollar*104.8+saudi_riyal*28);
+document.write("<h1> Currency in PKR</h1>")
+document.write( "Total Currency in PKR:" +pkr) */
+
+
+// Q10 
+
+
+/* var number= 2+5*10/2;
+ document.write(number); */
+
+
+//  Q11 
+
+
+ /* var current_year=2021;
+ var birth_year=1997;
+ var age_1=current_year-birth_year;
+var age_2=current_year-birth_year-1;
+ document.write("Age is Either " +age_1+ " or " +age_2+ " Years old ");  */
+
+
+//  Q12 
+  
+
+ /* var radius= 20;
+ var circumference= 2*3.14*20;
+ var area=3.14*20*20
+ document.write(" <h1> The Geometrizer </h1> ")
+ document.write("<p> Radius of a circle: " +radius+ " </p> ");
+ document.write("<p>The Circumference is:" +circumference+ " </p> ");
+ document.write("<p> The area is: " +area+ " </p> "); */
+
+//  Q13 
+
+
+ /* var fvt_snack="chocolate chip";
+ var curren_age= 15;
+ var max_age= 65;
+ var snack_per_day= 3;
+ var total= (365*snack_per_day)*(max_age-curren_age);
+ document.write(" <h1>The life Time Supply Calculator</h1> ");
+ document.write("<P> Current age: "+ curren_age +" </P> ");
+ document.write("<P> Estimated Maximum Age: "+ max_age +" </P> ");
+ document.write("<P> Amount of snack per day: "+ snack_per_day +" </P>" );
+ document.write("<P> You will need "+ total +" chocolate chip to last you until the ripe old age of 65</P>"); */
+
+//  Chapter 6 to 9 (Math Expressions
+
+
+//  Q1 
+
+
+ /* var a = 10;
+ document.write("<P> The value of  ++a is  "+ ++a + ": <p/>");  
+ document.write("<P> The value of  a is  "+ a + ": <p/>");  
+ document.write("<P> The value of  a++ is  "+ a++ + ": <p/>"); 
+ document.write("<P> The value of  a is  "+ a + ": <p/>"); 
+ document.write("<P> The value of  --a is  "+ --a + ": <p/>"); 
+ document.write("<P> The value of  a is  "+ a + ": <p/>"); 
+ document.write("<P> The value of  a-- is  "+ a-- + ": <p/>"); 
+ document.write("<P> The value of  a is  "+ a + ": <p/>");  */
+
+
+// Q2 
+
+
+/*  var a= 2, b= 1;
+var result= --a - --b + ++b + b--;
+document.write(result); */
+
+// Q3 
+
+
+/* var person = prompt("Please enter your name");
+
+document.write("Welcome " + person); */
+
+// Q4,5   
+
+
+
+/* var no = prompt("Please enter your number");
+var i;
+
+
+for(i=1;i<11;i++)
+{
+    document.write(no+" * "+i+" = "+no*i+"<br>")
+} */
+
+
+
+// Chapter 9 to 11 (User input and conditional statment)
+
+
+// Q1 
+
+
+/* var person = prompt("Please enter your city name");
+
+if (person == "karachi") {
+   
+  x="welcome to the city of lights"; 
+}
+document.write(x); */
+
+// Q2 
+
+
+/* var person = prompt("Enter your Gender");
+
+if (person === "male") {
+   
+  x="Good Morning Sir"; 
+}
+else if(person === "female") {
+   
+    x="Good Morning mam "; 
+  }
+  document.write(x); */
+
+
+// Q3
+
+  
+/* var person = prompt("Signal Color");
+
+if (person === "red") {
+   
+  x="Must stop"; 
+}
+ if(person === "yellow") {
+   
+    x="Ready to Move "; 
+  }
+
+ if(person === "green") {
+   
+    x="Move on "; 
+  }
+  document.write(x); */
+
+
+  // Q4 
+
+
+  /* var person = prompt("Fuel in Car");
+  if (person <0.25) {
+      x=" Please refill the fuel in your car ";
+
+  }
+  if (person == 0.25) {
+    x=" Your fuel is about to end ";
+
+}
+  document.write(x); */
+
+
+// Q5 
+
+
+/* var a = 4;
+if (++a === 5){
+alert("given condition for variable a is true");
+}  
+
+var b = 82;
+if (b++ === 83){
+alert("given condition for variable b is true");
+} 
+
+var c = 12;
+if (c++ === 13){
+alert("condition 1 is true");
+}
+if (c === 13){
+alert("condition 2 is true");
+}
+if (++c < 14){
+alert("condition 3 is true");
+}
+if(c === 14){
+alert("condition 4 is true");
+} 
+
+
+ var materialCost = 20000;
+var laborCost = 2000;
+var totalCost = materialCost + laborCost;
+if (totalCost === laborCost + materialCost){
+alert("The cost equals");
+} 
+
+
+if (true){
+    alert("True");
+    } 
+    if (false){
+    alert("False");
+    } 
+
+    if("car" < "cat"){
+        alert("car is smaller than cat");
+        } */
+
+
+  // Q7 
+
+
+/* var n= prompt("Guess the secret Number");
+m=2
+if (n == m )  {
+    x=" Bingo Correct answer "
+
+}
+else if (n==m+1)
+{
+    x="Close enough to the correct answer"
+}
+document.write(x); */
+
+
+  // Q8 
+
+
+ /* var n= prompt( "put a number");
+ if  ( n%3==0 ) {
+     x="Given Number is divisible by 3 "
+ }
+else {
+    x="Given number is not divisible by 3"
+}
+
+document.write(x); */
+
+
+// Q9 
+
+ 
+/* var n= prompt("Even or odd number");
+if ( n%2==0) {
+    x="Even Number"
+}
+else {
+x="Odd Number"
+}
+document.write(x); */
+
+
+  // Q10 
+
+
+/* var t= prompt("Temperature");
+if (t >40)  {
+    x=" it is too hot outside "
+
+}
+else if (t >30)  {
+    x=" The Weather Today is Nomral"
+
+}
+else if (t >20)  {
+    x=" Today's weather is cool"
+
+}
+
+else if (t >10)  {
+    x=" OMG! today's weather is so cool  "
+
+}
+else {
+x="Wrong value"
+}
+document.write(x); */
+
+
+// Q11 
+
+
+var n1=prompt("Enter first no");
+var n2=prompt("Enter 2nd no");
+var op=prompt("Enter \n 1 for Addition \n 2 for subtraction \n 3 for division \n 4 for multiplication \n 5 for modules");
+if(Number(op)===1)
+{document.write("Sum = "+(Number(n1)+Number(n2)));
+}
+if(Number(op)===2)
+{document.write("Difference = "+ (Number(n1)-Number(n2)));
+}
+if(Number(op)===3)
+{document.write("multiplication = "+(Number(n1)/Number(n2)));
+}
+if(Number(op)===4)
+{document.write("Division= "+(Number(n1)*Number(n2)));
+}
+if(Number(op)===5)
+{document.write("Modules= "+(Number(n1)*Number(n2)));
+}
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
